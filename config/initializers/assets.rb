774be713795d4c20cdaea5d6e2523ff7f6cd8bc7
nvaldebenito <nvaldebenito@localhost.localdomain>
2019-01-21 22:02:17 -0300

    add 50%

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,21 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bootstrap', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts', 'font-awesome', 'css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'ultimate-flat-social-icons', 'ultm-css')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images', 'testimonials')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images', 'screenshots')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images', 'team')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images', 'modal')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images', 'backgrounds')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'js')
+Rails.application.config.assets.precompile += %w(*.jpg)
+Rails.application.config.assets.precompile += %w(*.png)
+Rails.application.config.assets.precompile += %w(*.gif)
+
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
